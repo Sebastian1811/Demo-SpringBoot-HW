cba8e34d7262b6020cd8729a87294db341a6734f
sofiaponteb <aaponte@unal.edu.co>
2022-09-08 15:26:28 -0500

    Creacion microservicios

---
+++ backend/Adios/src/main/java/com/example/demo/AdiosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdiosApplication.class, args);
+	}
+
+}
